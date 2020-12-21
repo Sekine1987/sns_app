d0fcaaee3ea9b056ee2be6953a328e17cd98cb08
sekine <tempest.20.87@gmail.com>
2020-12-21 22:08:23 +0900

    new app create

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SnsApp
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+
+    config.encoding = "utf-8"
+  end
+end
